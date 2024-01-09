4ffb19c960fe918e330fa482f5fa5b63222f2bdd
Visionotup <124854703+McKingN@users.noreply.github.com>
2024-01-09 23:45:23 +0100

    try fix graph

--- GANRec/GANMF.py
+++ GANRec/GANMF.py
@@ -74,7 +74,7 @@ class GANMF(BaseRecommender):
                 real_loss = tf.reduce_mean(tf.abs(input_data - decoding))
                 fake_loss = tf.reduce_mean(tf.abs(input_data - tf.layers.dense(encoding, units=input_data.shape[1], 
                                                                                 kernel_initializer=glorot_uniform,
-                                                                                name='decoding_fake_loss')))
+                                                                                name='decoding')))
                 loss = real_loss - k_t * fake_loss
 
                 # Add diversity term to the loss function
