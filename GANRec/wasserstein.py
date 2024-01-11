3f77de0623609f79260189716e06c5608cabcf45
Visionotup <124854703+McKingN@users.noreply.github.com>
2024-01-11 15:31:13 +0100

    nbvc

--- GANRec/wasserstein.py
+++ GANRec/wasserstein.py
@@ -16,8 +16,8 @@ def autoencoder_wasserstein(input_data, decoding):
     # # Définir une opération tensorflow pour mettre à jour lambda
     # update_lambda = tf.assign(LAMBDA, tf.maximum(LAMBDA * lambda_decay, lambda_min))
 
-    def discriminator(x):
-        with tf.variable_scope('discriminator', reuse=False):
+    def discriminator(x, reuse=False):
+        with tf.variable_scope('discriminator', reuse=reuse):
             # h1 = tf.layers.dense(x, units=64, activation='relu', name='h1')
             # h2 = tf.layers.dense(h1, units=32, activation='relu', name='h2')
             score = tf.layers.dense(x, units=1, name='score')
@@ -25,13 +25,13 @@ def autoencoder_wasserstein(input_data, decoding):
     # Calculer le score du discriminateur pour input_data
     D_X = discriminator(input_data)
     # Calculer le score du discriminateur pour decoding
-    D_decoding = discriminator(decoding)
+    D_decoding = discriminator(decoding, reuse=True)
     # Générer des valeurs aléatoires entre 0 et 1
     epsilon = tf.random_uniform(shape=tf.shape(input_data), minval=0., maxval=1.)
     # Calculer le terme x_hat
     x_hat = epsilon * input_data + (1.0 - epsilon) * decoding
     # Calculer le score du discriminateur pour x_hat
-    D_X_hat = discriminator(x_hat)
+    D_X_hat = discriminator(x_hat, reuse=True)
     # Calculer le gradient du score du discriminateur par rapport à x_hat
     grad_D_X_hat = tf.gradients(D_X_hat, [x_hat])[0]
     # Calculer la norme du gradient
