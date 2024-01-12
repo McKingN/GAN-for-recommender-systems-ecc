ac8d2e2aed08ae58846c3dd74ff0c27dd9078158
Visionotup <124854703+McKingN@users.noreply.github.com>
2024-01-12 15:25:17 +0100

    zdzfef

--- GANRec/GANMF.py
+++ GANRec/GANMF.py
@@ -168,7 +168,7 @@ class GANMF(BaseRecommender):
         # losses
         # dloss = real_recon_loss + tf.maximum(0.0, m * real_recon_loss - fake_recon_loss) + \
         #         d_reg * tf.add_n([tf.nn.l2_loss(var) for var in self.params['D']])
-        dloss = real_recon_loss + self.wasserstein_disc(real_data=real_profile, fake_data=fake_profile, real_recon_loss=real_recon_loss, fake_recon_loss=fake_recon_loss, batch_size=batch_size) + \
+        dloss = real_recon_loss + self.wasserstein_disc(real_data=real_encoding, fake_data=fake_encoding, real_recon_loss=real_recon_loss, fake_recon_loss=fake_recon_loss, batch_size=batch_size) + \
                 d_reg * tf.add_n([tf.nn.l2_loss(var) for var in self.params['D']])
         gloss = (1 - recon_coefficient) * fake_recon_loss + \
                 recon_coefficient * self.wasserstein(real_data=real_profile, fake_data=fake_profile, real_encoding=real_encoding, fake_encoding=fake_encoding, batch_size=batch_size) + \
