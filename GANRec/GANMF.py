2c9f8e0eb1358a868910f9267d57c52a0748a104
Visionotup <124854703+McKingN@users.noreply.github.com>
2024-01-12 17:11:18 +0100

    giibkj

--- GANRec/GANMF.py
+++ GANRec/GANMF.py
@@ -67,11 +67,14 @@ class GANMF(BaseRecommender):
                 decoding = tf.layers.dense(encoding, units=self.num_items, kernel_initializer=glorot_uniform,
                                            name='decoding')
             # loss = tf.losses.mean_squared_error(input_data, decoding)
-            loss = tf.maximum(tf.reduce_mean(tf.nn.relu(input_data))-tf.reduce_mean(tf.nn.relu(decoding)),
-                              tf.reduce_mean(tf.nn.sigmoid(input_data))-tf.reduce_mean(tf.nn.sigmoid(decoding)),
-                              tf.reduce_mean(tf.nn.tanh(input_data))-tf.reduce_mean(tf.nn.tanh(decoding)),
-                              tf.reduce_mean(tf.nn.softmax(input_data))-tf.reduce_mean(tf.nn.softmax(decoding)),
-                              tf.losses.mean_squared_error(input_data, decoding))
+            losses = [
+                tf.reduce_mean(tf.nn.relu(input_data))-tf.reduce_mean(tf.nn.relu(decoding)),
+                tf.reduce_mean(tf.nn.sigmoid(input_data))-tf.reduce_mean(tf.nn.sigmoid(decoding)),
+                tf.reduce_mean(tf.nn.tanh(input_data))-tf.reduce_mean(tf.nn.tanh(decoding)),
+                tf.reduce_mean(tf.nn.softmax(input_data))-tf.reduce_mean(tf.nn.softmax(decoding)),
+                tf.losses.mean_squared_error(input_data, decoding)
+            ]
+            loss = tf.reduce_max(losses)
             # loss = tf.losses.hinge_loss(input_data, decoding)
             # loss = -tf.reduce_mean(input_data) + tf.reduce_mean(decoding)
             # loss = -tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=input_data, logits=decoding))
