c21b21281e37a4fc7b651fc1fefdefa9caed4679
Visionotup <124854703+McKingN@users.noreply.github.com>
2024-01-12 15:14:48 +0100

    zdf

--- GANRec/GANMF.py
+++ GANRec/GANMF.py
@@ -106,8 +106,8 @@ class GANMF(BaseRecommender):
         disc_cost += LAMBDA*gradient_penalty
         return disc_cost
     
-    def wasserstein_disc(self, real_data, fake_data, real_encoding, fake_encoding, batch_size):
-        disc_cost = tf.reduce_mean(fake_encoding) - tf.reduce_mean(real_encoding)
+    def wasserstein_disc(self, real_data, fake_data, real_recon_loss, fake_recon_loss, batch_size):
+        disc_cost = tf.maximum(0.0, tf.reduce_mean(fake_recon_loss) - tf.reduce_mean(real_recon_loss))
         alpha = tf.random_uniform(
                             shape=[batch_size,1], 
                             minval=0.,
@@ -168,7 +168,7 @@ class GANMF(BaseRecommender):
         # losses
         # dloss = real_recon_loss + tf.maximum(0.0, m * real_recon_loss - fake_recon_loss) + \
         #         d_reg * tf.add_n([tf.nn.l2_loss(var) for var in self.params['D']])
-        dloss = real_recon_loss + self.wasserstein_disc(real_data=real_profile, fake_data=fake_profile, real_encoding=real_encoding, fake_encoding=fake_encoding, batch_size=batch_size) + \
+        dloss = real_recon_loss + self.wasserstein_disc(real_data=real_profile, fake_data=fake_profile, real_recon_loss=real_recon_loss, fake_recon_loss=fake_recon_loss, batch_size=batch_size) + \
                 d_reg * tf.add_n([tf.nn.l2_loss(var) for var in self.params['D']])
         gloss = (1 - recon_coefficient) * fake_recon_loss + \
                 recon_coefficient * self.wasserstein(real_data=real_profile, fake_data=fake_profile, real_encoding=real_encoding, fake_encoding=fake_encoding, batch_size=batch_size) + \
